7109105eb4ada2c9d47dced8a06cd8fd5fe7b234
Lea Verou <lea@verou.me>
2023-02-07 13:33:20 -0500

    [grade-calculator] Convert to use createApp()

--- grade-calculator/index.js
+++ grade-calculator/index.js
@@ -1,34 +1,49 @@
-import "https://mavue.mavo.io/mavue.js";
+import { createApp } from "https://mavue.mavo.io/mavue.js";
 
-grade_calc.computed = {
-	/**
-	 * Final grade in the class
-	 */
-	calculatedGrade () {
-		let p = this.participation;
-		return 0.15 * p.studio + 0.05 * p.feedback + 0.05 * p.other + .75 * this.homeworkAverage;
+let app = createApp({
+	data: {
+		participation: {
+			studio: null,
+			feedback: null,
+			other: null
+		},
+		homeworks: [null]
 	},
 
-	/**
-	 * Returns the average of all homeworks that have been graded.
-	 */
-	homeworkAverage () {
-		let done = 0;
-		let sum = 0;
+	computed: {
+		/**
+		 * Final grade in the class (number)
+		 */
+		calculatedGrade () {
+			let p = this.participation;
+			return 0.15 * p.studio + 0.05 * p.feedback + 0.05 * p.other + .75 * this.homeworkAverage;
+		},
 
-		for (let hw of this.homeworks) {
-			if (hw >= 0) {
-				sum += hw;
-				done++;
+		/**
+		 * Returns the average of all homeworks that have been graded (number)
+		 */
+		homeworkAverage () {
+			let done = 0;
+			let sum = 0;
+
+			for (let hw of this.homeworks) {
+				if (hw >= 0) {
+					sum += hw;
+					done++;
+				}
 			}
+
+			return sum / done;
 		}
+	},
 
-		return sum / done;
+	methods: {
+		/**
+		 * Add a new blank homework	to the list.
+		 * Does not prevent more homeworks than 12 from being added.
+		 */
+		addHomework (i) {
+			this.homeworks.push(null);
+		}
 	}
-}
-
-grade_calc.methods = {
-	addHomework (i) {
-		this.homeworks.push(null);
-	}
-};
+}, "#grade_calc");
